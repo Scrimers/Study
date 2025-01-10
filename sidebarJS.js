297199350458e15065512510c4da54acd4b1d61e
Scrimers <jsnokin@naver.com>
2025-01-10 19:12:00 +0900

    UI 변경중

--- sidebarJS.js
+++ sidebarJS.js
@@ -55,10 +55,10 @@ for(var i=0 ; i<jsonData.length ; i++) {
 
 //사이드바 영역에 커서 올렸을 때
 document.querySelector("#sidebar").onmouseover = function() {
-    document.querySelector("#sidebarImg").setAttribute("src", "/image/drop_gray_left.png");
+    document.querySelector("#sidebarImg").setAttribute("src", "./image/drop_gray_left.png");
 }
 
 //사이드바 영역에 커서 나갔을 때
 document.querySelector("#sidebar").onmouseout = function() {
-    document.querySelector("#sidebarImg").setAttribute("src", "/image/drop_gray_right.png");
+    document.querySelector("#sidebarImg").setAttribute("src", "./image/drop_gray_right.png");
 }
