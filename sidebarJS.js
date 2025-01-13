65ffff26c2eed3e8a14661023660e9166bd23fd7
Scrimers <jsnokin@naver.com>
2025-01-13 13:53:19 +0900

    UI 변경중

--- sidebarJS.js
+++ sidebarJS.js
@@ -53,19 +53,15 @@ for(var i=0 ; i<jsonData.length ; i++) {
 
 //사이드바 내용물 클릭 시 해당 영역으로 이동
 
-/* 추후 수정후 복구
-//사이드바 영역에 커서 올렸을 때
-document.querySelector("#sidebar").onmouseover = function() {
-    document.querySelector("#sidebarImg").setAttribute("src", "./image/drop_gray_left.png");
-    // document.body.style.overflowY = 'hidden';
-}
+// //사이드바 영역에 커서 올렸을 때
+// document.querySelector("#sidebar").onmouseover = function() {
+//     document.querySelector("#sidebarImg").setAttribute("src", "./image/drop_gray_left.png");
+// }
 
-//사이드바 영역에 커서 나갔을 때
-document.querySelector("#sidebar").onmouseout = function() {
-    document.querySelector("#sidebarImg").setAttribute("src", "./image/drop_gray_right.png");
-    // document.body.style.overflowY = 'auto';
-}
-*/
+// //사이드바 영역에 커서 나갔을 때
+// document.querySelector("#sidebar").onmouseout = function() {
+//     document.querySelector("#sidebarImg").setAttribute("src", "./image/drop_gray_right.png");
+// }
 
 
 
@@ -86,12 +82,12 @@ document.querySelector('#sidebar > #sidebarMain > ul').addEventListener('mouseov
 
     //기존에 mouseover한 2depth가 있을 때
     if(mouseTarget.length > 0) {
-        //기존 mouseleave 이벤트 제거
-    // document.querySelector('.on').removeEventListener('mouseleave', sidebarMouseleave);
+        //사이드바 mouseleave 로직 호출
+        sidebarMouseleave();
     }    
 
     //새롭게 mouseover된 요소
-    mouseTarget = document.querySelectorAll('[aria-describedby="'+event.target.id+'"]');
+    mouseTarget = document.querySelectorAll('[data-groupNm="'+event.target.id+'"]');
 
     //요소에 2depth가 있을 때
     if(mouseTarget.length > 0) {
@@ -102,53 +98,33 @@ document.querySelector('#sidebar > #sidebarMain > ul').addEventListener('mouseov
             target.style.display = 'block';
         });
 
-        //sidebarMouseleave = 함수의 참조를 전달한다. 그래서 mouseleave 이벤트가 발생할 때 handleMouseLeave 함수가 호출된다.
-        //sidebarMouseleave() = 함수가 즉시 호출되어 그 반환값이 addEventListener에 전달된다. 
-        //그래서 mouseleave 이벤트가 발생하기 전에 handleMouseLeave 함수가 실행되고, 그 결과가 이벤트 핸들러로 등록됩니다. 
-        //만약 handleMouseLeave가 반환값이 없다면 undefined가 전달되므로, 이벤트가 발생해도 아무런 동작을 하지 않게 됩니다.
+        //addEventListener('mouseleave', sidebarMouseleave) = 함수의 참조를 전달한다. 그래서 mouseleave 이벤트가 발생할 때 sidebarMouseleave 함수가 호출된다.
+        //addEventListener('mouseleave', sidebarMouseleave()) = 함수가 즉시 호출되어 그 반환값이 addEventListener에 전달된다. 
+        //그래서 mouseleave 이벤트가 발생하기 전에 sidebarMouseleave 함수가 실행되고, 그 결과가 이벤트 핸들러로 등록된다. 
+        //만약 sidebarMouseleave가 반환값이 없다면 undefined가 전달되므로, 이벤트가 발생해도 아무런 동작을 하지 않게 된다.
         document.querySelector('.on').addEventListener('mouseleave', sidebarMouseleave);
-        /*
-        //class on 영역에서 커서 벗어날 시
-        document.querySelector('.on').addEventListener('mouseleave', function(event) {  
-            
-            // //기존 class on 제거
-            // document.querySelector('.on').setAttribute('class', '');
-            // //2depth요소 미노출 처리
-            // mouseTarget.forEach(function(target, index) {
-            //     target.style.display = 'none';
-            // });
-            // //기존 mouseleave 이벤트 제거
-            // document.querySelector('.on').removeEventListener('mouseleave', function(event) {});
-        });
-        */
     }
 });
 
+//메인 사이드바 스크롤을 서브 사이드바가 따라오게 하기
+const aDiv = document.getElementById('sidebarMain');
+const bDiv = document.getElementById('sidebarSub');
+aDiv.addEventListener('scroll', () => {
+    bDiv.scrollTop = aDiv.scrollTop; //a div의 스크롤 위치를 b div에 동기화
+});
+
+//사이드바 mouseleave 로직
 function sidebarMouseleave() {
-    console.log(1);
+    let onClass = document.querySelector('.on');
+    
     //기존 mouseleave 이벤트 제거
-    document.querySelector('.on').removeEventListener('mouseleave', sidebarMouseleave);
-    console.log(2);
+    onClass.removeEventListener('mouseleave', sidebarMouseleave);
     //기존 class on 제거
-    document.querySelector('.on').setAttribute('class', '');
-    console.log(3);
+    onClass.setAttribute('class', '');
     //2depth요소 미노출 처리
     mouseTarget.forEach(function(target, index) {
-        console.log(4);
         target.style.display = 'none';
     });
-    console.log(5);
+    //기존 mouseover 요소 초기화
+    mouseTarget = "";
 }
-
-// document.querySelector('.on').addEventListener('mouseleave', function(event) {
-//     console.log("나감 : ",event.target);
-//     // if(mouseTarget != "") {
-//     //     mouseTarget.forEach(function(target, index) {
-//     //         target.style.display = 'none';
-//     //     });
-//     // }
-// });
-
-// document.querySelector('#sidebarDiv').addEventListener('mouseleave', function(event) {
-//     console.log("완전히 나감 : ",event.target);
-// });
