4188b84d2137e4d0629494390134d27addd56719
Scrimers <jsnokin@naver.com>
2025-03-06 07:20:24 +0900

    리액트 공부중

--- react_auto/react_project/src/App.js
+++ react_auto/react_project/src/App.js
@@ -1,16 +1,15 @@
-function MyButton() {
+//App.js 파일
+
+//App2.js 파일에서 export한 MyButton함수를 MyButton7이라는 이름으로 사용할 것을 정의
+import MyButton7 from './App2';  
+
+function App() {
+  //MyButton7 함수 호출
   return (
-  <button>
-  I'm a button
-  </button>
+    <div className="123">
+      <MyButton7 />
+    </div>
   );
-  }
-  
-  export default function MyApp() {
-  return (
-  <div>
-  <h1>Welcome to my app</h1>
-  <MyButton />
-  </div>
-  );
-  }
+}
+
+export default App;
---
+++ react_auto/react_project/src/App2.js
@@ -0,0 +1,8 @@
+//App2.js 파일
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+export default MyButton;  //MyButton 함수 내보냄.
