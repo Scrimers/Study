c331f626f1b417ea4ef5d62de5db8f7201f5e2a4
Scrimers <jsnokin@naver.com>
2025-03-11 06:14:51 +0900

    리액트 공부중

--- react_auto/react_project/src/App.js
+++ react_auto/react_project/src/App.js
@@ -1,15 +1,78 @@
-//App.js 파일
+import './App.css';
 
-//App2.js 파일에서 export한 MyButton함수를 MyButton7이라는 이름으로 사용할 것을 정의
-import MyButton7 from './App2';  
+const jsonData = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+];
 
-function App() {
-  //MyButton7 함수 호출
-  return (
-    <div className="123">
-      <MyButton7 />
-    </div>
+function SearchComp() {
+  function textChange() {
+    
+  }
+  return(
+    <>
+    <input className="searchInput" placeholder="Search..." onChange={textChange}/>
+    <input type="checkbox"/>
+    Only show products in stock
+    <br /> <br />
+    </>
   );
 }
 
-export default App;
+function TabelMainComp() {
+  return(
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <TabelSubComp />
+      </tbody>
+    </table>
+  );
+}
+
+function TabelSubComp() {
+  var flag;
+  var htmlTag = [];
+
+  jsonData.forEach(function(x, index) {
+    if(index == 0 || (flag != x.category)) {
+      flag = x.category;
+      htmlTag.push(
+        <tr>
+          <th colspan="2">{x.category}</th>
+        </tr>
+      );  
+    }
+    
+    htmlTag.push(
+      <tr>
+        <td className={x.stocked || "noStocked"}>{x.name}</td>
+        <td>{x.price}</td>
+      </tr>
+    );
+  });
+
+  return(
+    <>    
+    {htmlTag}
+    </>
+  );
+}
+
+export default function MainComp() {
+  return (
+    <>
+    <SearchComp />
+    <TabelMainComp />
+    </>
+  );
+}
