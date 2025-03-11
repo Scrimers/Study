28089216de6db9d176f48c28b3ebfa286e618177
Scrimers <jsnokin@naver.com>
2025-03-12 05:45:43 +0900

    리액트 공부중

--- react_auto/react_project/src/App.js
+++ react_auto/react_project/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 
 const jsonData = [
   { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
@@ -9,21 +10,20 @@ const jsonData = [
   { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
 ];
 
-function SearchComp() {
-  function textChange() {
-    
-  }
+//조건 영역 컴포넌트
+function SearchComp({changeFunc, clickFunc}) {
   return(
     <>
-    <input className="searchInput" placeholder="Search..." onChange={textChange}/>
-    <input type="checkbox"/>
+    <input className="searchInput" placeholder="Search..." onChange={(e) => {changeFunc(e)}}/>
+    <input type="checkbox" onClick={(e) => {clickFunc(e)}}/>
     Only show products in stock
     <br /> <br />
     </>
   );
 }
 
-function TabelMainComp() {
+//테이블 헤더 컴포넌트
+function TabelMainComp({searchText, isChecked}) {
   return(
     <table>
       <thead>
@@ -33,22 +33,50 @@ function TabelMainComp() {
         </tr>
       </thead>
       <tbody>
-        <TabelSubComp />
+        <TabelSubComp searchText={searchText} isChecked={isChecked}/>
       </tbody>
     </table>
   );
 }
 
-function TabelSubComp() {
+//테이블 데이터 컴포넌트
+function TabelSubComp({searchText, isChecked}) {
   var flag;
   var htmlTag = [];
 
-  jsonData.forEach(function(x, index) {
+  //글자 필터
+  const jsonSearchFilter = jsonData.filter(function(x) {
+    let result;
+    if(searchText.trim() == "") {
+      result = x.name;
+    }
+    else {
+      result = x.name.toLowerCase().includes(searchText.trim().toLowerCase());
+    }
+    
+    return result;
+  });
+
+  //체크 필터
+  const jsonCheckedFilter = jsonSearchFilter.filter(function(x) {
+    let result;
+    if(isChecked == true) {
+      result = x.stocked == true;
+    }
+    else {
+      result = x.name;
+    }
+    
+    return result;
+  });
+
+  //반복문을 통해 데이터 영역 구성
+  jsonCheckedFilter.forEach(function(x, index) {
     if(index == 0 || (flag != x.category)) {
       flag = x.category;
       htmlTag.push(
         <tr>
-          <th colspan="2">{x.category}</th>
+          <th colSpan="2">{x.category}</th>
         </tr>
       );  
     }
@@ -68,11 +96,25 @@ function TabelSubComp() {
   );
 }
 
-export default function MainComp() {
+//메인 컴포넌트
+export default function MainComp() {  
+  const [searchText, setSearchText] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+
+  //글자 변경 이벤트
+  function changeFunc(e) {
+    setSearchText(e.target.value);
+  }
+
+  //체크박스 이벤트
+  function clickFunc(e) {
+    setIsChecked(e.target.checked);
+  }
+
   return (
     <>
-    <SearchComp />
-    <TabelMainComp />
+    <SearchComp changeFunc={changeFunc} clickFunc={clickFunc}/>
+    <TabelMainComp searchText={searchText} isChecked={isChecked}/>
     </>
   );
 }
