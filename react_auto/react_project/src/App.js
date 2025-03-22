3dd0e762bfd47163666d3b907baffadf9669bb8e
Scrimers <jsnokin@naver.com>
2025-03-23 03:17:52 +0900

    리액트 공부중

--- react_auto/react_project/src/App.js
+++ react_auto/react_project/src/App.js
@@ -1,30 +1,34 @@
-function Card( {children} ) {
+function Drink({ name }) {
+  let plant = 'leaf';
+  let content = '15–70 mg/cup';
+  let Age = '4,000+ years';
+
+  if(name !== 'tea') {
+    plant = 'bean';
+    content = '80–185 mg/cup';
+    Age = '1,000+ years';
+  }
+
   return (
-    <div className="card">
-      <div className="card-content">
-        {children}
-      </div>
-    </div>
+    <section>
+      <h1>{name}</h1>
+      <dl>
+        <dt>Part of plant</dt>
+        <dd>{plant}</dd>
+        <dt>Caffeine content</dt>
+        <dd>{content}</dd>
+        <dt>Age</dt>
+        <dd>{Age}</dd>
+      </dl>
+    </section>
   );
 }
 
-export default function Profile() {
+export default function DrinkList() {
   return (
     <div>
-      <Card>
-        <h1>Photo</h1>
-        <img
-          className="avatar"
-          src="https://i.imgur.com/OKS67lhm.jpg"
-          alt="Aklilu Lemma"
-          width={70}
-          height={70}
-        />
-      </Card>
-      <Card>
-      <h1>About</h1>
-      <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-      </Card>
+      <Drink name="tea" />
+      <Drink name="coffee" />
     </div>
   );
 }
