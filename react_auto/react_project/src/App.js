791a214b723e6f7feb853f61c22b2550e1550eb6
Scrimers <jsnokin@naver.com>
2025-03-29 03:47:03 +0900

    리액트 공부중

--- react_auto/react_project/src/App.js
+++ react_auto/react_project/src/App.js
@@ -1,34 +1,9 @@
-function Drink({ name }) {
-  let plant = 'leaf';
-  let content = '15–70 mg/cup';
-  let Age = '4,000+ years';
+import { recipes } from './data.js';
 
-  if(name !== 'tea') {
-    plant = 'bean';
-    content = '80–185 mg/cup';
-    Age = '1,000+ years';
-  }
-
-  return (
-    <section>
-      <h1>{name}</h1>
-      <dl>
-        <dt>Part of plant</dt>
-        <dd>{plant}</dd>
-        <dt>Caffeine content</dt>
-        <dd>{content}</dd>
-        <dt>Age</dt>
-        <dd>{Age}</dd>
-      </dl>
-    </section>
-  );
-}
-
-export default function DrinkList() {
+export default function RecipeList() {
   return (
     <div>
-      <Drink name="tea" />
-      <Drink name="coffee" />
+      <h1>Recipes</h1>
     </div>
   );
 }
